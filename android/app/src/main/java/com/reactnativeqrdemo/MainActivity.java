cf46743ad078d42f90bf2117c7f91a10506033d1
Umar Hambaran <omar.hambaran@frogasia.com>
2020-04-07 15:06:21 +0800

    chore: update right name for this project

--- android/app/src/main/java/com/reactnativeqrdemo/MainActivity.java
+++ android/app/src/main/java/com/reactnativeqrdemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativetemplatedemo;
+package com.reactnativeqrdemo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeTemplateDemo";
+    return "React Native QR Demo";
   }
 }
