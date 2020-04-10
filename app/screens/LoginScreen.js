380a786975605f51e609f0e3e407997e2d92a07f
Umar Hambaran <omar.hambaran@frogasia.com>
2020-04-10 17:05:26 +0800

    feat: wip feat login

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -2,19 +2,43 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  TextInput
 } from 'react-native';
 
 export default class LoginScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>LoginScreen component</Text>
+        <Text style={styles.text}>QR Code Demo</Text>
+        <View>
+          <Text>Enter username</Text>
+          <TextInput style={styles.username} />
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    alignContent: 'center',
+    paddingTop: '10%',
+    backgroundColor: 'white'
+  },
+  text: {
+    fontSize: 30,
+    backgroundColor: 'aliceblue',
+    padding: 20,
+    borderRadius: 10,
+    borderBottomWidth: 1,
+    borderTopWidth: 1
+  },
+  username: {
+    padding: 10,
+    width: 30,
+    borderWidth: 1
+  }
 });
--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -2,17 +2,56 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 export default class SplashScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      greetingMsg: ''
+    };
+  }
+
   componentDidMount() {
-    this.props.navigation.navigate('SplashScreen');
+    var self = this;
+    setTimeout(() => {
+      this.setState({
+        ...this.state,
+        greetingMsg: 'Greeting, Earthling ..'
+      }, () => {
+        setTimeout(() => {
+          self.props.navigation.navigate('Login');
+        }, 1500);
+      })
+    }, 500);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>SplashScreen component</Text>
+        <Text style={styles.text}>QR Code Demo</Text>
+        <Text style={styles.greetingMsg}>{this.state.greetingMsg}</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    alignContent: 'center',
+    paddingTop: '50%',
+    backgroundColor: 'white'
+  },
+  text: {
+    fontSize: 30,
+    backgroundColor: 'aliceblue',
+    padding: 20,
+    borderRadius: 10,
+    borderBottomWidth: 1,
+    borderTopWidth: 1
+  },
+  greetingMsg: {
+    marginTop: 20,
+    fontSize: 15
+  }
+});
